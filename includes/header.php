7475167adf8e854e72041715ca579edc8a58b25d
Ishaka Musa <ishakabukswe2019@gmail.com>
2024-02-23 07:57:46 +0000

    files

---
+++ includes/header.php
@@ -0,0 +1,73 @@
+<div class="navbar navbar-inverse set-radius-zero" style="background-color: #48B1D8;;" >
+<p style="text-align: center; font-size: 30px; margin-top: 25px; color: white; padding: 0px ">ONLINE LIBRARY BORROWING AND RETURNING SYSTEM</p>
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand">
+
+                </a>
+
+            </div>
+<?php if($_SESSION['login'])
+{
+?> 
+            <div class="right-div">
+                <a href="logout.php" class="btn btn-danger pull-right">LOG ME OUT</a>
+            </div>
+            <?php }?>
+        </div>
+    </div>
+    
+<?php if($_SESSION['login'])
+{
+?>    
+<section style="background-color: skyblue;;">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-left">
+                            <li><a href="dashboard.php" class="menu-top-active">DASHBOARD</a></li>
+                            <li><a href="issued-books.php">Issued Books</a></li>
+                             <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Account <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="my-profile.php">My Profile</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="change-password.php">Change Password</a></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <?php } else { ?>
+       
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav"  style="display: flex; justify-content: center; margin-left:350px; padding: 0; list-style: none;">                        
+                          
+      <li><a href="index.php">Home</a></li>
+      <li><a href="index.php#ulogin">User Login</a></li>
+                            <li><a href="signup.php">User Signup</a></li>
+                         
+                            <li><a href="adminlogin.php">Admin Login</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <?php } ?>
